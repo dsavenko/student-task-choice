e742d769992428b78ecfc579e3143368d21ea624
Dmitry Savenko <dsav@dsav-macbook.local>
2023-02-16 22:22:45 +0700

    Initial commit

---
+++ src/ru/nsu/dsavenko/taskchoice/TaskChoice.java
@@ -0,0 +1,50 @@
+package ru.nsu.dsavenko.taskchoice;
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class TaskChoice {
+
+    private static final List<String> STUDENTS = Arrays.asList(
+            "Агапудова Елизавета",
+            "Воробьев Андрей",
+            "Долгушин Василий",
+            "Егоров Данил",
+            "Клочихина Софья",
+            "Кондренко Кирилл",
+            "Кравец Дмитрий",
+            "Неретин Степан",
+            "Силицкий Андрей",
+            "Сироткин Михаил"
+    );
+
+    public static void main(String[] args) {
+        System.out.print("Количество задач: ");
+        int n = new Scanner(System.in).nextInt();
+        if (0 >= n) {
+            System.err.println("Должно быть больше 0");
+            return;
+        }
+        List<Integer> choice = createChoice(n, STUDENTS.size());
+        IntStream.range(0, STUDENTS.size())
+                .mapToObj(i -> STUDENTS.get(i) + " -> " + choice.get(i))
+                .forEach(System.out::println);
+    }
+
+    private static List<Integer> createChoice(int maxN) {
+        List<Integer> ret = IntStream.rangeClosed(1, maxN)
+                .boxed()
+                .collect(Collectors.toCollection(ArrayList::new));
+        Collections.shuffle(ret);
+        return ret;
+    }
+
+    private static List<Integer> createChoice(int maxN, int size) {
+        List<Integer> ret = new ArrayList<>();
+        while (ret.size() < size) {
+            ret.addAll(createChoice(maxN));
+        }
+        return ret;
+    }
+}
